36d67d314e87028fc2a9ee5f61af2538b3b80286
Aldrsd <aldi@trimatrastudio.com>
2022-09-17 20:20:49 +0700

    Expo Ready to Update

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
+import { AuthProvider } from './hooks/useAuth';
 import StackNavigator from './StackNavigator';
 
 export default function App() {
--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -6,29 +6,32 @@ const AuthContext = createContext({})
 const config = {
     iosClientId: '398467169577-l2kmjsts9luf34j9jcdqvo57khatdap6.apps.googleusercontent.com',
     androidClientId: '398467169577-eg9c6m7bdhsgueo8b7lu9reh2d50ftdb.apps.googleusercontent.com',
-    scopes: ["prifile", "email"],
+    scopes: ["profile", "email"],
     permissions: ["public_profile", "email", "gender", "location"]
 }
 
-var a = 10;
-var b = "Test";
 
 export const AuthProvider  = ({ children }) => {
 
     const signInWithGoogle = async()=>{
-        await Google.logInAsync()
-    }
+        await Google.logInAsync(config).then(async(logInResult)=>{
+            if(logInResult.type === "success"){
+                //login
+            }           
+        });
+    };
 
     return (
     <AuthContext.Provider
         value={{
             user: null,
+            signInWithGoogle
     } 
     }>
         {children}
     </AuthContext.Provider>
-    )
-}
+    );
+};
 
 export default function useAuth(){
     return useContext(AuthContext);
