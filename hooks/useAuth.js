98d19d6d724675412d49380aa2989b40c01e4226
Aldrsd <aldi@trimatrastudio.com>
2022-09-17 22:58:29 +0700

    Login with google

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -1,31 +1,39 @@
 import { View, Text } from 'react-native'
 import React, { createContext, useContext } from 'react'
-import * as Google from "expo-google-app-auth";
+import * as Google from "expo-auth-session/providers/google";
+import * as WebBrowser from "expo-web-browser";
+
+WebBrowser.maybeCompleteAuthSession();
 
 const AuthContext = createContext({})
-const config = {
-    iosClientId: '398467169577-l2kmjsts9luf34j9jcdqvo57khatdap6.apps.googleusercontent.com',
-    androidClientId: '398467169577-eg9c6m7bdhsgueo8b7lu9reh2d50ftdb.apps.googleusercontent.com',
-    scopes: ["profile", "email"],
-    permissions: ["public_profile", "email", "gender", "location"]
-}
-
 
 export const AuthProvider  = ({ children }) => {
 
-    const signInWithGoogle = async()=>{
-        await Google.logInAsync(config).then(async(logInResult)=>{
-            if(logInResult.type === "success"){
-                //login
-            }           
-        });
-    };
+    const [accesToken, setAccesToken] = React.useState();
+    const [userInfo, setUserInfo] = React.useState();
+    const [request, response, propmtAsync] = Google.useAuthRequest({
+        iosClientId: '398467169577-l2kmjsts9luf34j9jcdqvo57khatdap6.apps.googleusercontent.com',
+        androidClientId: '398467169577-eg9c6m7bdhsgueo8b7lu9reh2d50ftdb.apps.googleusercontent.com',
+        expoClientId:'398467169577-bqpj1p5rc7ktdfbeo3vda9fr6opnu0ak.apps.googleusercontent.com'
+    });
+    React.useEffect(()=>{
+        if(response?.type==="success"){
+            setAccesToken(response.authentication.accessToken);
+        }
+    }, [response]);
+
+    async function getUserData(){
+        let userInfoResponse = await fetch("https://www.googleapis.com/userinfo/v2/me", {
+            headers:{Authorization:`Bearer ${accessToken}`}
+        })
+    }
+
 
     return (
     <AuthContext.Provider
         value={{
-            user: null,
-            signInWithGoogle
+            //user: false,
+            propmtAsync,
     } 
     }>
         {children}
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,15 +1,15 @@
 import { View, Text, Button } from 'react-native'
 import React from 'react'
-import useAuth from '../hooks/useAuth';
+import useAuth from '../hooks/useAuth'
 
 const LoginScreen = () => {
-    const{signInWithGoogle } = useAuth();
+    const{propmtAsync}=useAuth();
     return (
     <View>
         <Text>LoginScreen</Text>
-        <Button title="login" onPress={signInWithGoogle}/>
+        <Button title='Login' onPress={propmtAsync()}/>
     </View>
-    );
-};
+    )
+}
 
 export default LoginScreen
