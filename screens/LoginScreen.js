8d29c005406d05d5286b181d0aa100e364e8e76c
Dikarahmanm <dhikarm78@gmail.com>
2022-09-20 01:01:09 +0700

    Build LoginScreen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,41 +1,43 @@
-import { View, Text, Button, ImageBackground } from "react-native";
+import {
+  View,
+  Text,
+  Button,
+  ImageBackground,
+  TouchableOpacity,
+} from "react-native";
 import React, { useLayoutEffect } from "react";
 import useAuth from "../hooks/useAuth";
 import { useNavigation } from "@react-navigation/core";
 import { styled, StyledComponent } from "nativewind";
 
 const LoginScreen = () => {
-  const{promptAsync, setLoading, loading }=useAuth();
+  const { promptAsync, setLoading, loading } = useAuth();
   const navigation = useNavigation();
 
   useLayoutEffect(() => {
     navigation.setOptions({
       headerShown: false,
     });
-  });
+  }, []);
 
   return (
     <View className="flex-1">
-      {/* <Text>{loading ? : "loading...." : "Login to the app"}</Text> */}
-      <Text>AAAA</Text>
-      <Text>AAAA</Text>
-      <Text>AAAA</Text>
-      <Text>AAAA</Text>
-      <Text>{loading? "Loading . . ." : "Login To The App"}</Text>
-      <Button title='login' onPress={()=>{
-        promptAsync()
-        setLoading(true)
-        }}/>
-      {/* <ImageBackground
+      <ImageBackground
         resizeMode="cover"
         className="flex-1"
         source={{
           uri: "https://tinder.com/static/tinder.png",
+          // SplashScreen Image
         }}>
-        <Text className="flex-1 items-center justify-center">
-          Sign in & get ASSSu
-        </Text>
-      </ImageBackground> */}
+        <TouchableOpacity
+          className="absolute bottom-20 w-52 bg-white p-3 rounded-2xl mx-12"
+          onPress={() => {
+            promptAsync();
+            setLoading(true);
+          }}>
+          <Text className="text-center">Sign in & get swiping</Text>
+        </TouchableOpacity>
+      </ImageBackground>
     </View>
   );
 };
