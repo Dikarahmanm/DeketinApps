44de5f5f232b45bf33ff5d2ba4b1cfbce3a95c2b
Dikarahmanm <dhikarm78@gmail.com>
2022-09-29 20:56:56 +0700

    Build Header

--- StackNavigator.js
+++ StackNavigator.js
@@ -13,17 +13,19 @@ const StackNavigator = () => {
   const { user } = useAuth();
   return (
     <Stack.Navigator
-      defaultScreenOptions={{
-        header: null,
+      ScreenOptions={{
+        headerShown: null,
       }}>
       {user ? (
         <>
           <Stack.Screen name="Home" component={HomeScreen} />
           <Stack.Screen name="Chat" component={ChatScreen} />
-          <Stack.Screen name="LoginPhone" component={LoginPhone} />
         </>
       ) : (
-        <Stack.Screen name="Login" component={LoginScreen} />
+        <>
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="LoginPhone" component={LoginPhone} />
+        </>
       )}
     </Stack.Navigator>
   );
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,26 +1,90 @@
-import { View, Text, Button } from "react-native";
+import {
+  View,
+  Text,
+  Button,
+  SafeAreaView,
+  TouchableOpacity,
+  Image,
+} from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 import useAuth from "../hooks/useAuth";
+import { AntDesign } from "@expo/vector-icons";
+import Swiper from "react-native-deck-swiper";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
-  const { logout } = useAuth();
-
+  const { user, logout } = useAuth();
   useLayoutEffect(() => {
     navigation.setOptions({
       headerShown: false,
     });
   }, []);
+
+  const DUMMY_DATA = [
+    {
+      firstName: "Rex",
+      distance: "5 miles",
+      age: "27",
+      photoURL:
+        "https://i.ibb.co/hKqkwXr/charlie-green-3-Jmf-ENc-L24-M-unsplash.jpg",
+    },
+    {
+      firstName: "Agung",
+      distance: "7 miles",
+      age: "21",
+      photoURL: "https://i.ibb.co/yVGGsJt/sigmund-a19-OVaa2rz-A-unsplash.jpg",
+    },
+    {
+      firstName: "Ucup",
+      distance: "12 miles",
+      age: "25",
+      photoURL: "https://i.ibb.co/W3Mf52h/nate-J5-U-22o1ubw-unsplash.jpg",
+    },
+  ];
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
-      <Button
-        title="Go to Chat Screen"
-        onPress={() => navigation.navigate("Chat")}
-      />
-      <Button title="Logout" onPress={() => logout()} />
-    </View>
+    <SafeAreaView>
+      {/*Header */}
+      <View>
+        <TouchableOpacity>
+          <Image
+            source={require("D:/Project Mobile Apps/DeketinApps/assets/Group9.png")}
+            style={{
+              top: "80%",
+              justifyContent: "center",
+              alignItems: "center",
+              marginLeft: "30%",
+            }}
+          />
+          <AntDesign
+            name="filter"
+            size={24}
+            color="black"
+            style={{
+              height: 34,
+              width: 34,
+              marginLeft: "85%",
+              top: "32%",
+            }}
+          />
+        </TouchableOpacity>
+      </View>
+
+      {/*End of Header */}
+
+      {/*CARDS */}
+      <View className="flex-1 -mt-6">
+        <Swiper
+          cards={DUMMY_DATA}
+          renderCard={(card) => {
+            <View>
+              <Text>{card.firstName}</Text>
+            </View>;
+          }}
+        />
+      </View>
+    </SafeAreaView>
   );
 };
 
